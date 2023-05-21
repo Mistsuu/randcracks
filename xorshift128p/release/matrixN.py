7fd94ac53fe49aeb676e035e702edae75bd0534c
Mistsuu <vietanhtran12@gmail.com>
2023-05-22 03:07:23 +0700

    Update code base for python

--- xorshift128p/release/matrixN.py
+++ xorshift128p/release/matrixN.py
@@ -138,69 +138,30 @@ def solve_right(M, v, nrows, ncols):
     return x
 
 def transpose(M, nrows, ncols):
-    cdef int C_nrows = nrows
-    cdef int C_ncols = ncols
-
-    cdef mpz_t* C_M = <mpz_t*> malloc(C_nrows * sizeof(mpz_t))
-    cdef mpz_t* C_R = <mpz_t*> malloc(C_ncols * sizeof(mpz_t))
-    for i_row in range(C_nrows):
-        C_M[i_row] = MPZ(M)
-    for i_row in range(C_ncols):
-        mpz_init_set_ui(C_R[i_row], 0)
-
-    for i_row in range(C_nrows):
-        for i_col in range(C_ncols):
-            if mpz_tstbit(C_M[i_row], i_col):
-                mpz_setbit(C_R[i_col], i_row)
-    R = _mpzs_to_GMPys(C_R, C_ncols)
-
-    # Cleanup
-    _mpzs_clear(C_R, C_ncols)
-    free(C_M)
-    free(C_R)
-    
-    # Return
+    R = [gmpy2.mpz(0)] * ncols
+    for i_row in range(nrows):
+        for i_col in range(ncols):
+            if gmpy2.bit_test(M[i_row], i_col):
+                R[i_col] = gmpy2.bit_set(R[i_col], i_row)
     return R
 
 def kernel_right(M, nrows, ncols):
-    cdef int C_nrows = nrows
-    cdef int C_ncols = ncols
-
     # Compute tranpose matrix M.T
-    cdef mpz_t* C_M  = <mpz_t*> malloc(C_nrows * sizeof(mpz_t))
-    cdef mpz_t* C_MT = <mpz_t*> malloc(C_ncols * sizeof(mpz_t))
-    for i_row in range(C_nrows):
-        C_M[i_row] = MPZ(M[i_row])
-    for i_row in range(C_ncols):
-        mpz_init_set_ui(C_MT[i_row], 0)
-    _transpose(C_M, C_MT, C_nrows, C_ncols)
+    MT = transpose(M, nrows, ncols)
 
     # Augment it with an ncols x ncols identity matrix on the right
-    for i_row in range(C_ncols):
-        mpz_setbit(C_MT[i_row], C_nrows + i_row)    
+    for i_row in range(ncols):
+        MT[i_row] = gmpy2.bit_set(MT[i_row], nrows + i_row)    
 
     # RREF it!
-    _rref_inplace(C_MT, C_ncols, C_nrows)
+    MT = rref(MT, ncols, nrows)
 
     # Get the left side of the augmented matrix
-    cdef mpz_t row_augL 
-    cdef mpz_t row_augR
-    mpz_init(row_augL)
-    mpz_init(row_augR)
-
     K = []
-    for i_row in range(C_ncols):
-        mpz_fdiv_r_2exp(row_augL, C_MT[i_row], C_nrows)
-        mpz_fdiv_q_2exp(row_augR, C_MT[i_row], C_nrows)
-        if mpz_size(row_augL) == 0 and mpz_size(row_augR) != 0:
-            K.append(GMPy_MPZ_From_mpz(row_augR))
-
-    # Cleanup
-    mpz_clear(row_augL)
-    mpz_clear(row_augR)
-    _mpzs_clear(C_MT, C_ncols)
-    free(C_M)
-    free(C_MT)
+    for i_row in range(ncols):
+        row_augR, row_augL = gmpy2.f_divmod_2exp(MT[i_row], nrows)
+        if row_augL == 0 and row_augR != 0:
+            K.append(row_augR)
 
     # Return unique
     return list(dict.fromkeys(K))
