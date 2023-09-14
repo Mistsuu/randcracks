9172342ff2982adb1cbc334a485df809c7b35774
Mistsuu <vietanhtran12@gmail.com>
2023-09-14 16:24:02 +0700

    Update accumulate_solve()

--- python_mt19937/release/mt19937_crackfun.py
+++ python_mt19937/release/mt19937_crackfun.py
@@ -7,38 +7,18 @@ def counting_to_100_with_randrange():
     rndSolver = RandomSolver()
     for i in range(100):
         rndSolver.submit_randrange(i, 0, 100, random.randrange(0, 3))
-    rndSolver.init_seed_states()
-    rndSolver.solve()
-
-    # Solving seed states before
-    # solving seed values helps
-    # finding seed faster.
-    z3_seed_states  = rndSolver.get_seed_states()
-    out_seed_states = rndSolver.get_skipped_variable_answer(z3_seed_states)
-    rndSolver.solver_constrants.extend([
-        z3_seed_state == out_seed_state for z3_seed_state, out_seed_state in zip(z3_seed_states, out_seed_states)
-    ])
-
-    rndSolver.init_seed_finder(624 * 32)
+    rndSolver.init_seed_states()                    # Solving seed states before
+    rndSolver.accumulate_solve()                    # solving seed values helps
+    rndSolver.init_seed_finder(624 * 32)            # finding seed faster.
     rndSolver.solve(force_redo=True)
     print(f'seed = {rndSolver.get_seed()}')
 
 def hello_world_with_randbytes():
     rndSolver = RandomSolver()
     rndSolver.submit_randbytes(b'Hello, world!')
-    rndSolver.init_seed_states()
-    rndSolver.solve()
-
-    # Solving seed states before
-    # solving seed values helps
-    # finding seed faster.
-    z3_seed_states  = rndSolver.get_seed_states()
-    out_seed_states = rndSolver.get_skipped_variable_answer(z3_seed_states)
-    rndSolver.solver_constrants.extend([
-        z3_seed_state == out_seed_state for z3_seed_state, out_seed_state in zip(z3_seed_states, out_seed_states)
-    ])
-
-    rndSolver.init_seed_finder(624 * 32)
+    rndSolver.init_seed_states()                    # Solving seed states before
+    rndSolver.accumulate_solve()                    # solving seed values helps
+    rndSolver.init_seed_finder(624 * 32)            # finding seed faster.
     rndSolver.solve(force_redo=True)
     print(f'seed = {rndSolver.get_seed()}')
 
